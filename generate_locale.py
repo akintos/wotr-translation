73d73fa24ebd92a20abc1ad0aba72afc78606b3d
Akintos <akintos47@gmail.com>
2022-11-02 03:03:39 +0900

    add locale gen script remove unused data

---
+++ generate_locale.py
@@ -0,0 +1,17 @@
+import glob
+import json
+
+from pf2bp import TranslationFile
+
+
+translation_data_dict: dict[str, str] = {}
+
+for filepath in glob.glob("translation/ko/*.po"):
+    tf = TranslationFile(filepath)
+    for unit in tf:
+        if unit.target:
+            translation_data_dict[unit.key] = unit.target
+
+
+with open("translation.json", "w", encoding="utf-8") as f:
+    json.dump(translation_data_dict, f, ensure_ascii=False, indent=2)
